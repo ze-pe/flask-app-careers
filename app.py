0caa47eba02e37e4ca3c9ee3a7497ae5c85f6671
centipede3 <>
2023-04-05 03:51:10 +0000

    added dynamic job pages, created and modified templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -8,8 +8,7 @@ def hello_world():
   jobs = load_jobs_from_db()
   return render_template(
     'home.html', 
-    jobs=jobs,
-    company_name='Careers'
+    jobs=jobs
   )
 
 @app.route("/api/jobs")
@@ -17,6 +16,13 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/jobs/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
+  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
     
--- database.py
+++ database.py
@@ -22,3 +22,13 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(zip(column_names, row)))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val").params(val=id))
+    column_names = result.keys()
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(zip(column_names, rows[0]))
